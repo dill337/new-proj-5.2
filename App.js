360d6305fbca7b6c6bc67308faacf70b24859d64
dill337 <64544519+dill337@users.noreply.github.com>
2020-12-28 16:17:59 -0600

    put on github

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, TextInput, View, Button, Alert, ScrollView } from 'react-native';
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+
+class HelloWorld extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: '', }
+  }
+
+  // alert the user input
+  alertMyText(input = []) {
+    Alert.alert(input.text);
+  }
+  render() {
+
+    return (
+
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Screen1"
+        >
+          <Stack.Screen
+            name="Screen1"
+            component={Screen1}
+          />
+          <Stack.Screen
+            name="Screen2"
+            component={Screen2}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+})
+
+export default HelloWorld
