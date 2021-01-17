06bc6f1441e5d79babefe84ba87b5f5c5c5dd753
dill337 <64544519+dill337@users.noreply.github.com>
2021-01-17 14:09:43 -0600

    added comments, jsdoc, & eslint

--- components/Start.js
+++ components/Start.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { Text, TextInput, View, ImageBackground, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import {
+  Text, TextInput, View, ImageBackground, TouchableOpacity, Button, StyleSheet,
+} from 'react-native';
 
-//const image = { uri: "https://reactjs.org/logo-og.png" };
 const backgroundImage = require('../assets/Background_Image.png');
 
-const backgroundColorOptions = ['#FFFFFF', '#474056', '#8A95A5', '#B9C6AE', '#FFFF00']
+const backgroundColorOptions = ['#FFFFFF', '#474056', '#8A95A5', '#B9C6AE', '#FFFF00'];
 
 class Start extends React.Component {
   constructor() {
@@ -26,7 +27,7 @@ class Start extends React.Component {
             // style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
             onChangeText={(name) => this.setState({ name })}
             value={this.state.name}
-            placeholder='Type name here ...'
+            placeholder="Type name here ..."
           />
           <View style={styles.chooseColorBox}>
             <Text style={styles.chooseColor}>
@@ -61,49 +62,47 @@ class Start extends React.Component {
             onPress={() => {
               this.props.navigation.navigate('Chat', {
                 name: this.state.name,
-                backgroundColor: this.state.backgroundColor
-              })
+                backgroundColor: this.state.backgroundColor,
+              });
             }}
           />
 
         </View>
       </ImageBackground>
-    )
+    );
   }
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column"
+    flexDirection: 'column',
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center"
+    resizeMode: 'cover',
+    justifyContent: 'center',
   },
   text: {
-    fontWeight: "bold",
-    color: "white",
+    fontWeight: 'bold',
+    color: 'white',
     fontSize: 32,
-    fontWeight: "bold",
-    textAlign: "center",
-    backgroundColor: "#000000a0"
+    textAlign: 'center',
+    backgroundColor: '#000000a0',
   },
   textInput: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    color: "#FFFFFF",
+    color: '#FFFFFF',
   },
   chooseColorBox: {
     alignSelf: 'flex-start',
     flex: 1,
     width: '100%',
-    // paddingLeft: 20,
     paddingBottom: '5%',
   },
   chooseColor: {
-    alignSelf: "flex-start",
+    alignSelf: 'flex-start',
     fontSize: 15,
     fontWeight: '300',
     color: '#000000',
@@ -114,9 +113,9 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignSelf: 'flex-start',
     width: '100%',
-    justifyContent: "space-around",
+    justifyContent: 'space-around',
     paddingLeft: 16,
-    marginTop: '3%'
+    marginTop: '3%',
   },
   colorSelector: {
     position: 'relative',
@@ -125,7 +124,7 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     margin: 2,
     borderColor: 'gray',
-  }
-})
+  },
+});
 
-export default Start
+export default Start;
